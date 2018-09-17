24395937f94bc3455cc512e1a6a6bfe2f4e5f3b7
Daniel Hunsaker <danhunsaker@gmail.com>
2018-09-17 00:27:56 -0600

    Content warnings and spoiler tags, media support
    Images (and other media) support warnings, too, so we need to handle them appropriately as well.

--- resources/views/vendor/medialibrary/responsiveImage.blade.php
+++ resources/views/vendor/medialibrary/responsiveImage.blade.php
@@ -1,1 +1,14 @@
-<img{!! $attributeString !!} srcset="{{ $media->getSrcset($conversion) }}" src="{{ $media->getUrl($conversion) }}">
+@if ($media->warnings()->count() > 0 || $media->spoilers()->count() > 0)
+    <div class="card warnings">
+        <div class="card-body">
+            @if ($media->warnings()->count() > 0)
+                @include('layouts.partials.content-warning', ['type' => $media->type, 'warnings' => $media->warnings()])
+            @endif
+
+            @if ($media->spoilers()->count() > 0)
+                @include('layouts.partials.spoiler-warning', ['type' => $media->type, 'spoilers' => $media->spoilers()])
+            @endif
+        </div>
+    </div>
+@endif
+<img{!! $attributeString !!} class="{{ $media->warnings()->count() > 0 ? ' warned' : '' }}{{ $media->spoilers()->count() > 0 ? ' spoiled' : '' }}" srcset="{{ $media->getSrcset($conversion) }}" src="{{ $media->getUrl($conversion) }}">
